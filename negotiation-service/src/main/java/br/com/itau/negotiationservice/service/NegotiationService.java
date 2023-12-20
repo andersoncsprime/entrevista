33be3fa972e96b8f5c41ecb670a4d62e5177eda2
Anderson Serdan <anderson.serdan@timwetech.com>
2023-12-20 11:34:43 -0300

    test entrevista

--- negotiation-service/src/main/java/br/com/itau/negotiationservice/service/NegotiationService.java
+++ negotiation-service/src/main/java/br/com/itau/negotiationservice/service/NegotiationService.java
@@ -1,10 +1,7 @@
 package br.com.itau.negotiationservice.service;
 
-import br.com.itau.negotiationservice.dto.AcordoDTO;
 import br.com.itau.negotiationservice.dto.ContratoDTO;
 import br.com.itau.negotiationservice.dto.DividaDTO;
-import br.com.itau.negotiationservice.dto.ProductDTO;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface NegotiationService {
